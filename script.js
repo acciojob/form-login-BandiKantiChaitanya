5e2d6b33eba95054b2635c4042bcafcf753ce298
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-19 13:36:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+let fname=document.querySelector('input[name="fname"]')
+let lname=document.querySelector('input[name="lname"]')
+
 function getFormvalue() {
     //Write your code here
+	let FirstName=fname.value
+	let LastName=lname.value
+	alert(`${FirstName} ${LastName}`)
 
 }
